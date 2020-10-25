e1679eb975f764d56bb82c1776635fba520ae377
tomhebunn <tomhebunn@gmail.com>
2020-10-25 16:11:16 +0000

    initial setup

--- skicast.js
+++ skicast.js
@@ -1,60 +1,99 @@
 // skicast.js
 
+const moment = require("moment");
+
 Module.register("skicast", {
+
 	// default module config
 	defaults: {
-		location: false,
-		locationID: false,
-		lat: false,
-		lon: false,
-		appid: '',
-		units = config.units,
-		updateInterval: 15 * 60 * 1000, // every 15 minutes
-		animationSpeed: 1000,
-		timeFormat: config.timeFormat,
-		lang: config.language,
-		decimalSymbol: '.',
-		fade: false,
-
-		initialLoaddelay: 2500, //2.5 seconds
+		updateInterval: 10 * 60 * 1000, // every 10 minutes
+		animationSpeed: 0,
+		initialLoadDelay: 2500, //2.5 seconds
 		retryDelay: 2500,
 
-		apiBase: 'https://api.weatherunlocked.com/',
+		maxWidth: "100%",
+		head: "true",
 
+		// weather unlocked information
+		appID: "8b6d0115",
+		apiKey: "1ebc12ef6c3877569787a4e3eac7c0a3",
+		resortId: "333005",
+		apiBase: 'https://api.weatherunlocked.com/api/snow/report/',
 		weatherProvider: 'weatherunlocked',
+
+		// styling
+		appendLocationNameToHeader: true,
+		calendarClass: "calendar",
+		tableClass: "small",
+		colored: false,
+		scale: false,
+
+		iconTable: {
+			"01d": "wi-day-sunny",
+			"02d": "wi-day-cloudy",
+			"03d": "wi-cloudy",
+			"04d": "wi-cloudy-windy",
+			"09d": "wi-showers",
+			"10d": "wi-rain",
+			"11d": "wi-thunderstorm",
+			"13d": "wi-snow",
+			"50d": "wi-fog",
+			"01n": "wi-night-clear",
+			"02n": "wi-night-cloudy",
+			"03n": "wi-night-cloudy",
+			"04n": "wi-night-cloudy",
+			"09n": "wi-night-showers",
+			"10n": "wi-night-rain",
+			"11n": "wi-night-thunderstorm",
+			"13n": "wi-night-snow",
+			"50n": "wi-night-alt-cloudy-windy"
+		},
+
 	},
 
-	getScripts: function () {
-		return ['moment.js']
+	getUrl: function () {
+		var url = null;
+
+		url = this.config.apiBase + this.config.resortId + "?app_id=" + this.config.appID + "&app_key=" + this.config.apiKey +".json";
 	},
 
-	start: function () {
-		Log.info("starting module: " + this.name);
+	getStyles: function () {
+		return ["skicast.css", "weathericons.css"]
+	},
 
-		//set locale
+	getScripts: function() {
+		return ["moment.js"]
+	},
+
+	// define start sequence
+	start: function() {
+		Log.info("Starting module: " + this.name);
+
+		// set locale
 		moment.locale(config.language);
 
-		this.forecast = [];
+		this.conditions = [];
 		this.loaded = false;
-		this.scheduleUpdate(this.config.initialLoaddelay);
+		this.scheduleUpdate(this.config.initialLoadDelay);
 
 		this.updateTimer = null;
 	},
 
-	// override dom generator
+	// Override dom generator
 	getDom: function () {
-		var wrapper = document.createElement('div');
+		var wrapper = document.createElement("div");
 
-		if (this.config.appid = "") {
-			wrapper.innerHTML = "Please set correct appid in config module";
-			wrapper.className = 'dimed light small';
-			return wrapper
+		var table = document.createElement("table");
+		table.classname = this.config.tableclass;
+
+		for (var c in this.conditions) {
+			var condition = this.conditions[c];
+
+			var row = document.createElement('tr');
+			if (this.config.colored) {
+				row.className = 'colored';
+			}
+			table.appendChild(row);
 		}
-	},
-
-	loaded: function(callback) {
-		this.finishloading();
-		Log.log(this.name + ' is loaded!');
-		callback();
 	}
 })
